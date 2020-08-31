dafb6458f0c8ad2c5178768d111220db1739b467
josephchamul <51219519+josephchamul@users.noreply.github.com>
2020-08-31 09:01:28 -0700

    Update question4.js

--- practicum/week-1/question4.js
+++ practicum/week-1/question4.js
@@ -1,15 +1,21 @@
 /*
 You are given strings A and B.  
 How many of the letters in B are also present in A?
-
 The letters in A are guaranteed distinct, and all characters in A and B are letters.
  Letters are case sensitive.
-
  Example:
  input: A='gtY', B = 'iTygrtg'
  output: 3
 */
 
 var howManyCommon = function(A, B) {
-
+    var same=0;
+    for(var i=0; i<B.length;i++){
+        for(var x=0; x<A.length; x++){
+            if (B[i] == A[x]){
+                same+=1;
+            }
+        }
+    }
+    return same;
 };
