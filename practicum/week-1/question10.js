3a48b63e987147b87e2415662822ee8c17ec9ea6
josephchamul <51219519+josephchamul@users.noreply.github.com>
2020-08-31 11:57:25 -0700

    Update question10.js

--- practicum/week-1/question10.js
+++ practicum/week-1/question10.js
@@ -7,5 +7,10 @@ output: 7 - 2 = 5
 */
 
 var maxMinusMin = function(n) {
-
+        var nums = [];
+        while (n>0){
+            nums.push(n%10);
+            n = parseInt(n/10);
+        }
+        return Math.max.apply(null, nums)-Math.min.apply(null, nums);
 };
