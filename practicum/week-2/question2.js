8e1cc53ce005931057724d1ba446409f0f68c809
josephchamul <51219519+josephchamul@users.noreply.github.com>
2020-09-06 09:48:24 -0700

    Update question2.js

--- practicum/week-2/question2.js
+++ practicum/week-2/question2.js
@@ -10,6 +10,19 @@ Example: input: [-12,3,-1,5,-2,1,-7]
           */
 
  var largestSubarray = function(input) {
-     
+    let maxNum = 0;
+    for (let i=0; i<input.length; i++){
+        for (let x=1; x<=input.length; x++){
+            let nums = 0;
+            var subarr = input.slice(i,x);
+            for (let val=0; val<subarr.length; val++){
+                nums += subarr[val];
+            }
+            if (nums > maxNum){
+                maxNum = nums;
+            }
+        }
+    }
+    return maxNum;
     
- };
+};
