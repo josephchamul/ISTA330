c96bd82db5e4420f7116d94ebb9d6ecd44b30ad7
josephchamul <51219519+josephchamul@users.noreply.github.com>
2020-09-06 14:10:55 -0700

    Update question8.js

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -13,5 +13,20 @@ output: 3
 */
 
 var d_integer = function(input) {
+    var nums = {};
+    let d = -1;
+    for (let i=0; i<input.length; i++){
+        if (input[i] in nums){
+            nums[input[i]] += 1;
+        }else{
+            nums[input[i]] = 1;
+        }
+    }
+    for (var key in nums){
+        if (key == nums[key]){
+            d = nums[key];
+        }
+    }
+    return d;
 
 };
