eafdb552f6f4ea2197d7a9e11915db31fc810c0c
josephchamul <51219519+josephchamul@users.noreply.github.com>
2020-09-07 08:22:01 -0700

    Update question12.js

--- practicum/week-2/question12.js
+++ practicum/week-2/question12.js
@@ -9,5 +9,9 @@ Given n, calculate F(n).
 */
 
 var F = function(n) {
-
+  var fib = [0,1]; 
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+  }
+  return fib[fib.length-1];
 };
