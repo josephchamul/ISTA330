2d525b6da902fb309b6e1bb9f52dac58d78c191a
josephchamul <51219519+josephchamul@users.noreply.github.com>
2020-09-14 08:27:42 -0700

    Update question2.js

--- practicum/week-3/question2.js
+++ practicum/week-3/question2.js
@@ -11,6 +11,20 @@ Example: input: [19,3,2,10,8,2,3,5]
           */
 
  var findDuplicates = function(input) {
-     
+     let dupl = {};
+     for (let i=0; i<input.length; i++){
+         if (input[i] in dupl){
+             dupl[input[i]] += 1;
+         }else{
+             dupl[input[i]] = 1;
+         }
+     }
+     let out = [];
+     for (let key in dupl){
+         if (dupl[key] == 2){
+             out.push(parseInt(key));
+         }
+     }
+     return out;
     
- };
+};
