933646dbae68a0c1ede872fdeb4cdfd2e06ffe47
josephchamul <51219519+josephchamul@users.noreply.github.com>
2020-09-16 17:14:13 -0700

    Update question5.js

--- practicum/week-3/question5.js
+++ practicum/week-3/question5.js
@@ -9,7 +9,23 @@ input: [1,2,3,4]
 output: [24,12,8,6]
 */
 
-var productOfOthers = function(input) {
+var productOfOthers = function (input) {
+  let left = [];
+  let right = [];
+  let product = [];
+  left[0] = 1;
 
+  for (let i=1; i<input.length; i++) {
+    left[i] = input[i-1]*left[i-1];
+  }
+  right[input.length-1] = 1;
+  for (let i=input.length-2; i>=0; i--) {
+    right[i] = input[i+1]*right[i+1];
+  }
+  for (let i=0; i<input.length; i++) {
+    product[i] = left[i]*right[i];
+  }
+
+  return product;
 };
 
